2492b2a9a35fa7529a72892aff16222e9d2258ec
Calvin Grewal <calvingrewal28@gmail.com>
2017-08-31 08:15:11 -0700

    can draw any graph

--- index.js
+++ index.js
@@ -1,84 +1,109 @@
-var svg = d3.select("svg"),
-  margin = {top: 20, right: 80, bottom: 30, left: 50},
-  width = svg.attr("width") - margin.left - margin.right,
-  height = svg.attr("height") - margin.top - margin.bottom,
-  g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function drawGraph(id, dataType) {
+  var svg = d3.select(id),
+    margin = {
+      top: 20,
+      right: 80,
+      bottom: 30,
+      left: 50
+    },
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var parseTime = d3.timeParse("%Y%m%d");
-var x = d3.scaleLinear().range([0, width]),
-  y = d3.scaleLinear().range([height, 0]),
-  z = d3.scaleOrdinal(d3.schemeCategory10);
+  var parseTime = d3.timeParse("%Y%m%d");
+  var x = d3.scaleLinear().range([0, width]),
+    y = d3.scaleLinear().range([height, 0]),
+    z = d3.scaleOrdinal(d3.schemeCategory10);
 
-var line = d3.line()
-  .x(function(d) { return x(d.passYds); })
-  .y(function(d) { return y(d.points); })
-  .curve(d3.curveBasis)
-
-d3.csv("data.csv", type, function(error, data) {
-  if (error) throw error;
-  const games = getGamesFromData(data)
-  console.log('list of games', games)
-
-  x.domain(d3.extent(games, function(d) { return d.passYds; }));
-
-  y.domain(d3.extent(games, function(d) { return d.points }));
-
-  z.domain(games.map(function(g, i) { return i; }));
-
-  g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .append('text')
-    .text('Passing Yards')
-    .attr('fill', '#000')
-    .attr('dy', '25px')
-    .attr('x', "150px")
-
-  g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y))
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "0.71em")
-    .attr("fill", "#000")
-    .text("Points");
-
-  g.append('g').append("path")
-    .datum(games)
-    .attr("class", "line")
-    .attr("d", function(d) {
-      const linepath = line(d)
-
-      return linepath;
+  var line = d3.line()
+    .x(function(d) {
+      return x(d.data);
     })
-    .style("stroke", function(d) { return '#0099cc'; }); //returns the color
+    .y(function(d) {
+      return y(d.points);
+    })
+    .curve(d3.curveLinear)
+
+  d3.csv("data.csv", type, function(error, data) {
+    if (error) throw error;
+    const games = getGamesFromData(data)
+
+    x.domain(d3.extent(games, function(d) {
+      return d.data;
+    }));
+
+    y.domain(d3.extent(games, function(d) {
+      return d.points
+    }));
+
+    z.domain(games.map(function(g, i) {
+      return i;
+    }));
+
+    g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .append('text')
+      .text(dataType)
+      .attr('fill', '#000')
+      .attr('dy', '25px')
+      .attr('x', "150px")
+
+    g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y))
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("fill", "#000")
+      .text("Points");
+
+    g.append('g').append("path")
+      .datum(games)
+      .attr("class", "line")
+      .attr("d", function(d) {
+        const linepath = line(d)
+
+        return linepath;
+      })
+      .style("stroke", function(d) {
+        return `hsl(${Math.floor(Math.random()*360)}, 100%, 40%)`;
+      }); //returns the color
 
 
-});
-
-function type(d, _, columns) {
-  d.date = parseTime(d.date);
-  d.Result = d.Result.split('-')[0].split(' ')[1]
-  for (var i = 1, n = columns.length, c; i < n; ++i) {
-    d[c = columns[i]] = isNaN(parseInt(d[c])) ? 0 : parseInt(d[c]) ;
-  }
-  return d;
-}
-function getGamesFromData(data) {
-
-  var games = data.map(function(game, i) {
-
-    return {
-      passYds: game.Yds,
-      points: game.Result,
-
-    }
-  }).sort(function(a, b) {
-    return a.passYds - b.passYds
   });
 
-  games.pop()
-  return games
+  function type(d, _, columns) {
+    d.date = parseTime(d.date);
+    d.Result = d.Result.split('-')[0].split(' ')[1]
+    for (var i = 1, n = columns.length, c; i < n; ++i) {
+      d[c = columns[i]] = isNaN(parseInt(d[c])) ? 0 : parseInt(d[c]);
+    }
+    return d;
   }
+
+  function getGamesFromData(data) {
+
+    var games = data.map(function(game, i) {
+
+      return {
+        data: game[dataType],
+        points: game.Result,
+
+      }
+    }).sort(function(a, b) {
+      return a.data - b.data
+    });
+
+    games.pop()
+    return games
+  }
+}
+
+drawGraph('#graph1', 'Yds')
+drawGraph('#graph2', 'Rate')
+drawGraph('#graph3', 'Int')
+drawGraph('#graph4', 'Cmp%')
+drawGraph('#graph5', 'TD')
